207379a0f209eff4a11b424275d417ab6633ff9b
YoukoKobayashi <86542067+YoukoKobayashi@users.noreply.github.com>
2022-10-30 14:23:46 +0900

    add_challenge
    チャレンジ問題追加

--- main.js
+++ main.js
@@ -1,49 +1,53 @@
 const addBtn =
     document.getElementsByClassName("add-btn");
 
-addBtn[0].addEventListener("click", () => {
+addBtn[0].addEventListener("click", function () {
     addListBtn();
     delListBtn();
 });
 
-const addListBtn = () => {
+const addListBtn = function () {
     const addText =
         document.getElementById("add-area");
     const listText = document.createTextNode(
         addText.value
     );
+    // challenge_add
+    if (addText.value === "") {
+        alert("空欄です");
+        return;
+    }
     const addLi = document.createElement("li");
-    addLi.classList.add("addList");
     addLi.appendChild(listText);
 
     const finishBtn =
         document.createElement("button");
     finishBtn.classList.add("finishBtn");
     finishBtn.innerHTML = "完了";
+
     addLi.appendChild(finishBtn);
+    // ここで削除機能を追加するという方法のほうが簡単
 
     const lists = document.getElementById("todo");
     lists.appendChild(addLi);
+    // challenge_add
+    addText.value = "";
 };
 
-const delListBtn = () => {
+const delListBtn = function () {
     const finishBtn =
         document.getElementsByClassName(
             "finishBtn"
         );
-    // const addList =
-    //     document.getElementsByClassName(
-    //         "addList"
-    //     );
     for (let n = 0; n < finishBtn.length; n++) {
         finishBtn[n].addEventListener(
             "click",
-            () => {
-                // addList[n].remove();
-                // finishBtn[n].remove();
-                const oneList =
-                    finishBtn[n].parentNode;
-                oneList.remove();
+            function () {
+                // finishBtn[n].remove();以下すべてのリストが削除される
+                // アロー関数で書くとthisの内容がundifinedになる
+                // すべてのアロー関数を無名関数に変更したらOK!
+                const addLi = this.parentNode;
+                addLi.remove();
             }
         );
     }
